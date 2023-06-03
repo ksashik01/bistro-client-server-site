6cb38c2bf122bbde4e6ae2ee85b1bcc2328b1005
ksashik01 <kscomplainsheet@gmail.com>
2023-06-03 13:02:02 +0600

    new file

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const usersCollection = client.db("bistroDb").collection("users");
 // user related apis---
 
 
-app.get ('/users', async (req, res) =>{
+app.get ('/users', verifyJWT, async (req, res) =>{
   const result = await usersCollection.find().toArray();
   res.send(result)
   })
@@ -82,11 +82,17 @@ app.get('/menu', async (req, res) =>{
 
 // Card Cooletion-------
 
-app.get('/carts', async (req, res) =>{
+app.get('/carts', verifyJWT, async (req, res) =>{
   const email = req.query.email;
   if (!email){
     res.send ([]);
   }
+
+  const decodedEmail = req.decoded.email;
+  if (email !== decodedEmail){
+    return res.status(403).send({error:true, message:'probidden access'})
+  }
+
 const query = {email: email};
 const result = await cartCollection.find(query).toArray();
 res.send(result)
@@ -130,12 +136,52 @@ app.patch('/users/admin/:id', async (req, res) => {
 
 
 // Jwt------
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
+
+
+
+
 app.post('/jwt', (req, res) => {
   const user = req.body;
   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
 
   res.send({ token })
 })
+
+// --------------------
+
+app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+  const email = req.params.email;
+
+  if (req.decoded.email !== email) {
+    res.send({ admin: false })
+  }
+
+  const query = { email: email }
+  const user = await usersCollection.findOne(query);
+  const result = { admin: user?.role === 'admin' }
+  res.send(result);
+})
+
+
 
 
 
